db371ae8ae0fe3ea26f8b68c03a399768d9d20e1
vitali <vital1234mail@gmail.com>
2020-11-25 22:17:22 +0300

    FIXED 5. Реализовать класс Tracker[396#252102] added test

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -14,4 +14,15 @@ public class TrackerTest {
         Item result = tracker.findById(item.getId());
         assertThat(result.getName(), is(item.getName()));
     }
+
+    @Test
+    public void checkFindAll() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item(1, "test1");
+        Item item2 = new Item(2, "test2");
+        Item item3 = null;
+        tracker.add(item1);
+        tracker.add(item2);
+        assertThat(tracker.findAll().length, is(2));
+    }
 }
