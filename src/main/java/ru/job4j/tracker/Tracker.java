7590f6ad5c2f342ce36b25b821a3fd30077e0b74
vitali <vital1234mail@gmail.com>
2020-11-27 19:41:06 +0300

    6. Метод замены заявки. Tracker.replace[211748#252100]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,24 +14,18 @@ public class Tracker {
     }
 
     public boolean replace(int id, Item item) {
-        if (id > items.length - 1) {
+        int replaceIndex = this.indexOf(id);
+        if (replaceIndex < 0) {
             return false;
         }
         item.setId(id);
-        items[id] = item;
+        items[replaceIndex] = item;
         return true;
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findByName(String key) {
@@ -51,4 +45,14 @@ public class Tracker {
         return Arrays.copyOf(items, size);
     }
 
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
 }
